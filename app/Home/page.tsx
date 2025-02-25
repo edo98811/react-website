2b32a4681b73b975ea6703dca46077f331294ab3
“edo98811” <filippie@uni-mainz.de>
2025-02-25 14:36:17 +0100

    update

---
+++ app/Home/page.tsx
@@ -0,0 +1,12 @@
+
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "../page";  // Your main page
+import CVPage from "../CV/page"; // Your new CV page
+
+export default function App() {
+  return (
+    <div>
+      <p>This page is under construction. Please check back later.</p>
+    </div>
+  );
+}
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
+  assetPrefix: isProd ? '/react-website/' : '',
+  basePath: isProd ? '/react-website' : '',
+  output: 'export'
 };
 
 export default nextConfig;
